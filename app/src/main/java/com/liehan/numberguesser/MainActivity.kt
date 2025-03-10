516588f38359720caab8d2e4a7bbfd2fd237e762
Liehan Els <liehan13@gmail.com>
2025-03-10 11:09:44 +0200

    Added more comments

--- app/src/main/java/com/liehan/numberguesser/MainActivity.kt
+++ app/src/main/java/com/liehan/numberguesser/MainActivity.kt
@@ -25,30 +25,38 @@ class MainActivity : AppCompatActivity() {
         //create a random number
         val randomNumber = Random.nextInt(1, 11)
 
-        //get the user guess and save it as an int
+        //connect tge backend to the frontend elements
         val userGuess = findViewById<EditText>(R.id.edtUserGuess)
         val tvAnswer = findViewById<TextView>(R.id.tvAnswer)
         val guessButton = findViewById<Button>(R.id.btnGuess)
+
+        //creates the button listener or click action
         guessButton.setOnClickListener {
+            //get the user guess from the element and saves it into a variable
             var guessNumberString = userGuess.text.toString()
+            //creates an empty integer variable
             var guessNumberInt: Int
             when {
+                //checks if the guess is a number
                 guessNumberString.toIntOrNull() != null -> {
+                    //if yes, converts the guess to an integer
                     guessNumberInt = guessNumberString.toInt()
-
                     //check guess against random number
                     if (guessNumberInt == randomNumber) {
+                        //if yes, displays a toast message
                         Toast.makeText(this, "You guessed correctly", Toast.LENGTH_SHORT).show()
                     } else {
+                        //if no, displays a toast message
                         Toast.makeText(this, "You guessed incorrectly", Toast.LENGTH_SHORT).show()
                     }
-                    //sets the tvAnswer to the random number
+                    //sets the tvAnswer to the random number if the guess was a number
                     tvAnswer.text = randomNumber.toString()
                 }
+                //if guess was not a number, displays a toast message
                 else -> {
                     Toast.makeText(this, "Please enter a number", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
+                } //end of else
+            } //end of when
+        } //end of guessButton.setOnClickListener
     } //end of onCreate
 } //end of Main
